60370633c4c88de58768922196add18c4b92d365
mc962 <11969409+mc962@users.noreply.github.com>
2021-08-19 20:06:25 -0400

    Add cors middleware

---
+++ config/initializers/cors.rb
@@ -0,0 +1,9 @@
+# TODO configure properly for production
+unless Rails.env.production?
+  Rails.application.config.middleware.insert_before 0, Rack::Cors do
+    allow do
+      origins '*'
+      resource '*', headers: :any, methods: [:get, :post, :patch, :put]
+    end
+  end
+end
