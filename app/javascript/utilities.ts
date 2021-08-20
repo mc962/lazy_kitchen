3de705b0d494d5504d25800c9fea12c9a59b4f01
mc962 <11969409+mc962@users.noreply.github.com>
2021-08-19 20:09:31 -0400

    Extract vue

--- app/javascript/utilities.ts
+++
@@ -1,3 +0,0 @@
-const randomNum = () => {
-  return window.crypto.getRandomValues(new Uint32Array(1))[0].toString(16);
-};
--- config/webpack/production.js
+++ config/webpack/production.js
@@ -3,12 +3,4 @@ process.env.NODE_ENV = process.env.NODE_ENV || 'production'
 const environment = require('./environment')
 const webpack = require('webpack')
 
-environment.plugins.append(
-    'DefinePlugin',
-    new webpack.DefinePlugin({
-        __VUE_OPTIONS_API__: true,
-        __VUE_PROD_DEVTOOLS__: false
-    })
-)
-
 module.exports = environment.toWebpackConfig()
