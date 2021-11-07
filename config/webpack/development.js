7672055ce242078e694e481985c9d4af9345728d
mc962 <11969409+mc962@users.noreply.github.com>
2021-11-07 09:47:02 -0500

    Add build analysis tools

--- config/webpack/development.js
+++ config/webpack/development.js
@@ -3,4 +3,20 @@ process.env.NODE_ENV = process.env.NODE_ENV || 'development'
 const environment = require('./environment')
 const webpack = require('webpack')
 
-module.exports = environment.toWebpackConfig()
+// Build Analysis Tools
+const CircularDependencyPlugin = require('circular-dependency-plugin')
+environment.plugins.append(
+    'CircularDependencyPlugin',
+    new CircularDependencyPlugin()
+)
+const BundleAnalyzerPlugin = require('webpack-bundle-analyzer').BundleAnalyzerPlugin;
+environment.plugins.append(
+    'BundleAnalyzerPlugin',
+    new BundleAnalyzerPlugin()
+)
+const SpeedMeasurePlugin = require("speed-measure-webpack-plugin");
+const smp = new SpeedMeasurePlugin();
+const webpackConfig = smp.wrap(environment.toWebpackConfig());
+module.exports = webpackConfig
+
+// module.exports = environment.toWebpackConfig()
