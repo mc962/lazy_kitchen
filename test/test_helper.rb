cbdd9ea27376acff354bcaa663359eb599f67039
mc962 <11969409+mc962@users.noreply.github.com>
2021-11-13 00:23:32 -0500

    Use spec reporter

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -1,6 +1,8 @@
 ENV['RAILS_ENV'] ||= 'test'
 require_relative "../config/environment"
 require "rails/test_help"
+require "minitest/reporters"
+Minitest::Reporters.use! [Minitest::Reporters::SpecReporter.new]
 
 class ActiveSupport::TestCase
   include FactoryBot::Syntax::Methods
