8e3d575d65ae65cd4b05a828e525e8c2c714aed7
mc962 <11969409+mc962@users.noreply.github.com>
2022-01-21 18:22:47 -0500

    Remove webpack configs

--- config/webpack/production.js
+++
@@ -1,6 +0,0 @@
-process.env.NODE_ENV = process.env.NODE_ENV || 'production'
-
-const environment = require('./environment')
-const webpack = require('webpack')
-
-module.exports = environment.toWebpackConfig()
