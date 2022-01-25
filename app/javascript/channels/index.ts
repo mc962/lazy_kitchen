ba2b5ac8faca4811136132671c3473ca3491a17f
mc962 <11969409+mc962@users.noreply.github.com>
2022-01-24 21:46:53 -0500

    Remove extra js code

--- app/javascript/channels/index.ts
+++
@@ -1,7 +0,0 @@
-// Load all the channels within this directory and all subdirectories.
-// Channel files must be named *_channel.js.
-
-export const setupChannels = () => {
-    // const channels = require.context('.', true, /_channel\.js$/)
-    // channels.keys().forEach(channels)
-}
