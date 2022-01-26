a735e727ca69d1f5a3f1e7acbf712587003a4739
mc962 <11969409+mc962@users.noreply.github.com>
2022-01-26 17:43:15 -0500

    Remove extra importmap pins

--- config/importmap.rb
+++ config/importmap.rb
@@ -1,6 +1,3 @@
 # Pin npm packages by running ./bin/importmap
 
 pin "application", preload: true
-pin "sortablejs", to: "https://ga.jspm.io/npm:sortablejs@1.14.0/modular/sortable.esm.js"
-pin "stimulus-rails-nested-form", to: "https://ga.jspm.io/npm:stimulus-rails-nested-form@4.0.0/dist/stimulus-rails-nested-form.es.js"
-pin "stimulus-sortable", to: "https://ga.jspm.io/npm:stimulus-sortable@3.1.0/dist/stimulus-sortable.es.js"
