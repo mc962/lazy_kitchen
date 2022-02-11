44b8f18793a4c24173de3bb0dc12d4c4dcfee7eb
mc962 <11969409+mc962@users.noreply.github.com>
2022-02-10 23:07:40 -0500

    Add docs and change text_pattern_ops ordering

--- db/migrate/20220210040240_add_ancestry_to_ingredients.rb
+++ db/migrate/20220210040240_add_ancestry_to_ingredients.rb
@@ -1,6 +1,7 @@
 class AddAncestryToIngredients < ActiveRecord::Migration[7.0]
   def change
     add_column :ingredients, :ancestry, :string
-    add_index :ingredients, :ancestry, order: {slug: :text_pattern_ops}
+    # text_pattern_ops is a recommended optimization of the ancestry column index: https://github.com/stefankroes/ancestry
+    add_index :ingredients, :ancestry, order: {ancestry: :text_pattern_ops}
   end
 end
