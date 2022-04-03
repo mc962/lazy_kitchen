f819759460d050a429bf0a7a5d3972f299f7aaea
mc962 <11969409+mc962@users.noreply.github.com>
2022-04-03 01:05:11 -0400

    Remove unnecessary tests

--- test/controllers/api/posts_controller_test.rb
+++ test/controllers/api/posts_controller_test.rb
@@ -1,13 +1,4 @@
 require "test_helper"
 
 class API::PostsControllerTest < ActionDispatch::IntegrationTest
-  test "should get index" do
-    get api_posts_index_url
-    assert_response :success
-  end
-
-  test "should get show" do
-    get api_posts_show_url
-    assert_response :success
-  end
 end
